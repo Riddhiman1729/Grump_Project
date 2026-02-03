56513a9dfce5ed281c2a99b1643318df5ed9b973
ZiyueZHENG <75109830+ZiyueZHENG@users.noreply.github.com>
2026-02-03 12:49:18 -0800

    adding X regression feature

--- R/EM.R
+++ R/EM.R
@@ -31,7 +31,7 @@ estimate_dirichlet <- function(X, w, maxiter = 100, tol = 1e-6) {
     # stop if converged
     if (max(abs(alpha - alpha_old)) < tol) break
   }
-  alpha
+  return(alpha)
 }
 
 # Helper: invert digamma(x) - Ref: Minka TP (2012)
@@ -42,18 +42,86 @@ digamma_inv <- function(x) {
   return(y)
 }
 
+
+# Helper: solve multinomial glm
+solve_multinom <- function(y, X, lambda = 0){
+  fit <- glmnet::glmnet(x = X,
+                        y = y,
+                        family = "multinomial",
+                        intercept = TRUE)
+  coefs = glmnet::coef.glmnet(fit, s = lambda)
+  return(as.matrix(do.call(cbind, coefs)))
+}
+
+
+# Helper: base spline
+solve_spline <- function(s){
+  if(ncols == 1){
+    
+  }else if(ncol(s) == 2){
+    s1 <- s[,1]
+    s2 <- s[,2]
+    basis1 <- create.bspline.basis(range(s1), nbasis=3, norder=4)
+    basis2 <- create.bspline.basis(range(s2), nbasis=3, norder=4)
+    
+    B1 <- eval.basis(s1, basis1)
+    B2 <- eval.basis(s2, basis2)
+    
+    # center columns to avoid intercept-like duplication
+    B1 <- scale(B1, center=TRUE, scale=FALSE)
+    B2 <- scale(B2, center=TRUE, scale=FALSE)
+    
+    B <- as.matrix(do.call(cbind, lapply(1:ncol(B1), function(a) B1[,a] * B2)))
+  }
+  return(B)
+}
+
+
+
+##### glmnet test (To be deleted) #####
+# X = matrix(c(1,2,3,5,2,1,3,1),ncol = 2)
+# y = matrix(c(0.3,0.6,0.1,0.2,0.6,0.2,0.4,0.2,0.4,0.3,0.5,0.2),ncol = 3,byrow = TRUE)
+# fit <- glmnet(x = X,
+#        y = y,
+#        family = "multinomial",
+#        intercept = TRUE)
+# coefs = coef(fit,s = fit$lambda[10])
+# as.matrix(do.call(cbind, coefs)) # Get coefficents
+# predict(fit, newx = X, s = fit$lambda[10], type = "response") # Make prediction
+##### end of test (To be deleted) #####
+
+
+##### spline version glmnet test (To be deleted) #####
+# library(splines) # Choice one
+# library(fda) # Choice two
+# s <- c(100,101,102,103) # spacial coeff
+# # Using splines package
+# B_sp <- splines::bs(s, df = 3, degree = 3, intercept = FALSE)
+# # Using fda package
+# basis <- fda::create.bspline.basis(range(s), nbasis = 6, norder = 4)  # cubic: norder=4
+# B_fda <- fda::eval.basis(s, basis)
+# B_fda <- scale(B_fda, center = TRUE, scale = FALSE)
+##### end of test (To be deleted) #####
+
+
+
+
+
+
 # Estep: Calculate the responsibilities
-e_step <- function(X, pi , theta){
-  n <- nrow(X)
-  K <- length(pi)
+e_step <- function(Y, pi , theta){
+  n <- nrow(Y)
+  K <- ncol(pi)
   
   log_w <- matrix(nrow = n,ncol = K)
   
   for(k in 1:K){
-    log_fk <- log_dirichlet(X, theta[[k]])
-    log_w[,k] <- log(pi[k]) + log_fk
+    log_fk <- log_dirichlet(Y, theta[[k]])
+    log_w[,k] <- log(pi[,k]) + log_fk
   }
-  log_denom <- log(rowSums(exp(log_w)))
+  
+  m <- apply(log_w,1,max)
+  log_denom <- m + log(rowSums(exp(log_w-m))) # replace log_denom <- log(rowSums(exp(log_w))) to aviod -log0
   resp <- exp(log_w - log_denom)
   
   return(resp)
@@ -61,53 +129,82 @@ e_step <- function(X, pi , theta){
 
 
 # Mstep: update the parameters
-m_step <- function(X, resp, theta){
-  n <- nrow(X)
+m_step <- function(Y, X = NULL, s = NULL, resp, theta, regress = FALSE){
+  n <- nrow(Y)
   K <- ncol(resp)
   
-  # Update p
-  Nk <- colSums(resp)
-  pi_new <- Nk/n
-  
+  # Update pi and beta
+  if(!regress){
+    Nk <- colSums(resp)
+    pi_new <- matrix(rep(Nk/n,50),ncol = 2,byrow = TRUE)
+    beta = NULL
+  }else{
+    if(is.null(X)){
+      print("X is missing")
+    }
+    
+    if(is.null(s)){
+      X_full = X
+    }else if(ncol(s) %in% c(1,2)){
+      B <- solve_spline(s)
+      X_full = cbind(X,B)
+    }else{
+      print("Spatial covariates dimension error")
+    }
+    
+    # only X corvariates
+    beta = solve_multinom(y = resp, X = X_full)
+    pi_new_mat = as.matrix(exp(cbind(1,X_full) %*% beta))
+    pi_new <- pi_new_mat / rowSums(pi_new_mat)
+    # X + f(s)
+    # X + f(s1,s2)
+  }
+
   # Update theta
   theta_new <- vector("list", K)
   for(k in 1:K){
-    theta_new[[k]] <- estimate_dirichlet(X, 
+    theta_new[[k]] <- estimate_dirichlet(Y, 
                                          resp[,k],
                                          maxiter = 1000,
                                          tol = 1e-6)
   }
   
-  return(list(pi = pi_new, theta = theta_new))
+  return(list(pi = pi_new, beta = beta, theta = theta_new))
 }
 
 
 # EM function
-EM_dirichlet <- function(X,
+EM_dirichlet <- function(Y,
+                         X = NULL,
+                         s = NULL,
                          nclust = 2,
                          max_iter = 1000,
                          tol = 1e-6,
+                         regress = FALSE,
                          verbose = FALSE){
-  n <- nrow(X)
-  d <- ncol(X)
+  n <- nrow(Y)
+  d <- ncol(Y)
   K <- nclust
   
   # Initialization
-  pi <- rep(1/K , K)
+  pi <- matrix(rep(1/K , K * n), nrow = n)
   theta <- vector("list", K)
   for(k in 1:K){
-    theta[[k]] <- sample(1:10,d)
+    samp <- sample.int(nrow(Y) , nrow(Y) %/% K )
+    theta[[k]] <- colSums(Y[samp,])   # need to think about the initialization
   }
   # loglik_trace <- rep(-Inf, max_iter)
   
   # EM algorithm
   for(iter in 1:max_iter){
+    if(iter %% 10 == 0){print(iter)} # print iter(TO BE DELETED)
     # E step
-    resp <- e_step(X, pi, theta)
+    resp <- e_step(Y, pi, theta)
     
     # M step
-    mstep_res <- m_step(X, resp , theta)
+    mstep_res <- m_step(Y = Y, X = X, s = s, resp , theta, regress = regress)
     pi_new <- mstep_res$pi
+    beta_new <- mstep_res$beta
     theta_new <- mstep_res$theta
     
     # Convergence check
@@ -118,11 +215,13 @@ EM_dirichlet <- function(X,
     
     # Update parameters
     pi <- pi_new
+    beta <- beta_new
     theta <- theta_new
   }
   
   return( list(
     pi = pi,
+    beta = beta,
     theta = theta,
     resp = resp
     #log_likeli <- log_likeli_trace
@@ -133,11 +232,11 @@ EM_dirichlet <- function(X,
 
 
 # Test
-theta1 <- c(2,5,3,4)
-theta2 <- c(8,2,1,3)
-X <- rbind(rdirichlet(1000,theta1),
-           rdirichlet(500,theta2))
-
-res <- EM_dirichlet(X, nclust = 2, max_iter = 100)
-res$pi
-res$theta
+# theta1 <- c(2,5,3,4)
+# theta2 <- c(8,2,1,3)
+# Y <- rbind(rdirichlet(1000,theta1),
+#            rdirichlet(500,theta2))
+# 
+# res <- EM_dirichlet(Y = Y, nclust = 2, max_iter = 100, regress = FALSE)
+# res$pi
+# res$theta
